140b6239bab2104e0b522abe1ee56f01425e93fd
Ivana3003 <iramljak84@gmail.com>
2026-08-28 10:07:57 +0200

    chore: configure restricted Firebase web API key

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+window.SUPPLEMENT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCkkcljweJ4emIBrIxWDIR79cqS1vmueXU",
+  authDomain: "supplement-tracker-cb744.firebaseapp.com",
+  projectId: "supplement-tracker-cb744",
+  appId: "1:377963764145:web:ae2ada76afcc2916724590",
+  storageBucket: "supplement-tracker-cb744.firebasestorage.app",
+  messagingSenderId: "377963764145",
+};
